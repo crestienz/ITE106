1ea229eda4c76c1491849201482c1fac222b912a
Christine Teleg <167881964+crestienz@users.noreply.github.com>
2024-10-16 20:44:34 +0800

    Update TextFileManipulationScannerLoop.java

--- 02TextFileManipulationScannerLoop/TextFileManipulationScannerLoop.java
+++ 02TextFileManipulationScannerLoop/TextFileManipulationScannerLoop.java
@@ -1,13 +1,12 @@
 import java.io.File;
 import java.util.Scanner;
-public class TextFileManipulationScannerWithLoop
-{
-	public static void main(String[] args) throws Exception
-	{
-	File file = new File("christineteleg.txt");
-	Scanner sc = new Scanner(file);
-	
-	while (sc.hasNextLine())
-		System.out.println(sc.nextLine());
-	}
+
+public class TextFileManipulationScannerLoop {
+    public static void main(String[] args) throws Exception {
+        File file = new File("C:\\Users\\pc1\\Desktop\\christine.txt");
+        Scanner sc = new Scanner(file);
+
+        while (sc.hasNextLine())
+            System.out.println(sc.nextLine());
+    }
 }
